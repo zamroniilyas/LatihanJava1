a916f3dcc542035a31c34439a87cc86c44586f8d
Zamroni Ilyas <frayzz.dmorgan@gmail.com>
2013-10-18 11:13:40 +0700

    Create Split.java

---
+++ Split.java
@@ -0,0 +1,11 @@
+public class Split {
+    public static void main(String[] args) {
+        String dir = "Rp.0";
+        String[] splt= dir.split("Rp.");
+        
+//        for (int i = 0; i < splt.length; i++) {
+//            System.out.println(splt[i]);
+//        }
+        System.out.println(splt[1]);
+    }
+}
